2409f1058d542c31fce34f18720940b43efbd522
Victor Utreras <vutreras@live.com>
2018-09-10 21:29:24 -0300

    Add button for create diagnostic pdf in admin section

---
+++ app/code/Transbank/Onepay/Controller/Adminhtml/Diagnostic/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace Transbank\Onepay\Controller\Adminhtml\Diagnostic;
+ 
+class Index extends \Magento\Backend\App\Action {
+
+    /**
+     * Render
+     * 
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute() {
+
+        die('Crear pdf de diagnóstico');
+    }
+}
