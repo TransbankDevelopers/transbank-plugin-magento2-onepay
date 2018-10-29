3363f0473a00fcae0ce2deac214d56c0baf79db8
Victor Utreras J <vutreras@live.com>
2018-10-29 10:41:45 -0300

    Update package.sh with new files and name of plugin

--- package.sh
+++ package.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
-zip -FSr transbank-onepay-1.0.1.zip . -x docs/\* *.git/\* .DS_Store* .editorconfig* .gitignore* .vscode/\* package.sh .travis* README.md README_EN.md *.zip
+#Script for create the plugin artifact
+echo "Travis tag: $TRAVIS_TAG"
+
+if [ "$TRAVIS_TAG" = "" ]
+then
+   TRAVIS_TAG='1.0.1'
+fi
+
+SRC_DIR="."
+FILE1="etc/module.xml"
+FILE2="composer.json"
+
+sed -i.bkp "s/1.0.1/${TRAVIS_TAG}/g" "$SRC_DIR/$FILE1" "$SRC_DIR/$FILE1"
+sed -i.bkp "s/\"version\": \"1.0.1\"/\"version\": \"${TRAVIS_TAG}\"/g" "$SRC_DIR/$FILE2" "$SRC_DIR/$FILE2"
+
+PLUGIN_FILE="plugin-transbank-onepay-magento2-$TRAVIS_TAG.zip"
+
+zip -FSr $PLUGIN_FILE . -x docs/\* *.git/\* .DS_Store* .editorconfig* .gitignore* .vscode/\* package.sh .travis* README.md *.zip docker-magento2/\* "$FILE1.bkp" "$FILE2.bkp"
+
+rm "$SRC_DIR/$FILE1.bkp"
+rm "$SRC_DIR/$FILE2.bkp"
+
+echo "Plugin version: $TRAVIS_TAG"
+echo "Plugin file: $PLUGIN_FILE"
